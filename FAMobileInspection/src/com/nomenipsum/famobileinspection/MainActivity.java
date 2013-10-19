dfb28ee7276847c6f288afe51da85d183c042d35
Philip Kurowski <pkurowsk@uwo.ca>
2013-10-18 23:02:26 -0400

    Base project code
    Two activities MainActivity.java and MainMenuActivity.java
    MainActivity.java navigates to MainMenuActivity.java and sends an Intent

---
+++ FAMobileInspection/src/com/nomenipsum/famobileinspection/MainActivity.java
@@ -0,0 +1,63 @@
+/*
+ * MainActivity.java
+ * The first page of the application.
+ * This page takes in a user's username and password,
+ * then authorizes the user and continues to the main menu
+ */
+
+package com.nomenipsum.famobileinspection;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+	Button btnLogin;					// The button that submits login credentials
+	EditText etUsername, 				// The EditText that takes in the user's username
+	etPassword;							// The EditText that takes in the user's password
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		// Obtain UI elements from the view by ID
+		btnLogin = (Button) findViewById(R.id.btnLogin);
+		etUsername = (EditText)findViewById(R.id.etUsername);
+		etPassword = (EditText)findViewById(R.id.etPassword);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	/** 
+	 * Called when login button is clicked is clicked
+	 * Sends user credentials to FA and continues to next screen if approved
+	 */
+	public void OnClickLogin(View v)	{
+		android.util.Log.d(v.toString(), "Login button pressed");
+		
+		String username = "username";
+		String password = "password";
+		if (etUsername.getText() != null)
+			username = etUsername.getText().toString();
+		if (etPassword.getText() != null)
+			password = etPassword.getText().toString();
+		
+		Intent intent = new Intent(this, MainMenuActivity.class);
+	    intent.putExtra("com.nomenipsum.famobileinspection.MESSAGE", username);
+	    startActivity(intent);
+
+	}
+	
+
+}
---
+++ FAMobileInspection/src/com/nomenipsum/famobileinspection/MainMenuActivity.java
@@ -0,0 +1,30 @@
+package com.nomenipsum.famobileinspection;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainMenuActivity extends Activity {
+	
+	TextView tvMainMenuTitle;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_menu);
+		
+		Intent intent = getIntent();
+		String message = intent.getStringExtra("com.nomenipsum.famobileinspection.MESSAGE");
+		tvMainMenuTitle = (TextView)findViewById(R.id.tvMainMenuTitle);
+		tvMainMenuTitle.setText("Welcome, " + message);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+}
